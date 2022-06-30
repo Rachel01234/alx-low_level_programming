437c19bb33616f747b3ebdb0940b7683402210b7
“Rachel01234cat README.mdgit config --global user.name “Rachel01234 <som63625@gmail.com>
2022-06-30 16:16:22 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
+
 /**
- * Main - print the string in the put function
- * use the main function
- * this program prints exactly with proper grammar, but the outcome is a piece of art,
+ * main - print the string in the put function
+ * prints exactly with proper grammar, but the outcome is a piece of art,
  * Return: 0
  */
 int main(void)
