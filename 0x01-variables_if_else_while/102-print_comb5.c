7ba9de6ce91c238c0171b898881f4085292ac03a
“Rachel01234cat README.mdgit config --global user.name “Rachel01234 <som63625@gmail.com>
2022-07-01 15:49:14 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,18 +3,18 @@
 #include <stdio.h>
 
 /**
- * main - two two-digit numbers
- * combinations of two 
+ * main - function
+ * combinations of two
  * two-digit numbers
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 
 {
 
 	int n, l;
-		 
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
@@ -33,6 +33,6 @@ int main(void)
 	if (last_digit == 0)
 	printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	if (last_digit < 6 && last_digit != 0)
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	printf("Last digit of %d is %d and less than 6 and not 0\n", n, last_digit);
 	return (0);
 }
