9bbb7fb908a61795e99a1f10b5156592c8bd4e96
“Rachel01234cat README.mdgit config --global user.name “Rachel01234 <som63625@gmail.com>
2022-07-05 15:25:22 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function the alphabet 10 times
+ * return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+
+	char a = 'a';
+
+	int n = 10;
+
+	while (n >= 1)
+	{
+	while (a <= 'z')
+	{
+	_putchar(a);
+	++a;
+	}
+	--n;
+	_putchar(10);
+	a = 'a';
+	}
+}
