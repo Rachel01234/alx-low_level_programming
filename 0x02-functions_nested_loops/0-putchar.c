8685587bdef3e283d101d90f357baeb72da4a916
“Rachel01234cat README.mdgit config --global user.name “Rachel01234 <som63625@gmail.com>
2022-07-05 12:58:37 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * main - prints the string _putchar from a character array.
- * Return: 0 on success.
- */
-
+ * main - main function
+ * Task zero
+ * Return: End program
+*/
 int main(void)
-
 {
+	char string[8] = "_putchar";
 
-	char c[] = "_putchar";
+	int n = 0;
 
-	int i;
-
-	for (i = 0; i < 9; i++)
+	while (n < 8)
 	{
-	_putchar(c[i]);
+	_putchar(string[n]);
+	++n;
 	}
-	_putchar('\n');
+	_putchar(10);
 	return (0);
 }
