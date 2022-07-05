162173c099800b4ce936e640b0ee20c3e0f07d52
“Rachel01234cat README.mdgit config --global user.name “Rachel01234 <som63625@gmail.com>
2022-07-05 15:56:27 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,10 @@
 
 /**
  * _isalpha - function to know if
- * the character is a letter or not
- * c: variable to evaluate
+ * @c: variable to evaluate
  * Return: Get 1 if it's a letter,
  * 0 otherwise
- */
+*/
 
 int _isalpha(int c)
 {
