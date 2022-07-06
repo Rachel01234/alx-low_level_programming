55093eebf9ac7d04b417788a9ef687dc81ec226c
“Rachel01234cat README.mdgit config --global user.name “Rachel01234 <som63625@gmail.com>
2022-07-05 19:54:50 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - this program will assign a random number to the variable n
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+*/
+
+int main(void)
+
+{
+	int i;
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
