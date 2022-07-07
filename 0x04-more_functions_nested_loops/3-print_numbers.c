d6364fa29ad47feea73905f2d36ef2f1014482e6
“Rachel01234cat README.mdgit config --global user.name “Rachel01234 <som63625@gmail.com>
2022-07-07 07:39:29 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,17 @@
 
 /**
  * print_numbers - function
+ * Description: Prints the numbers with _putchar
+ * Return: void
 */
 
 void print_numbers(void)
 {
 	int number;
+	int _putchar();
 
-	for (number = '0'; number <= '9'; number++)
+	for(number = '0'; number <= '9'; number++)
 	{
 	_putchar(number);
 	}
-	_putchar('\n');
 }
