42414cbdc01821c3944caa8627469b8c03c0f54c
“Rachel01234cat README.mdgit config --global user.name “Rachel01234 <som63625@gmail.com>
2022-07-11 10:55:00 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - function 
+ * prints a string, in reverse
+ * @s: pointer of parameter
+*/
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	length++;
+	while (length > 0)
+	_putchar(s[--length]);
+	_putchar(10);
+
+}
+
