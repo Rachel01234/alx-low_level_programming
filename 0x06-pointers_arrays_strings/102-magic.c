38ec6f88c8940496c076254e61e15e19e9a07804
“Rachel01234cat README.mdgit config --global user.name “Rachel01234 <som63625@gmail.com>
2022-07-13 15:33:24 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-it main(void)
+/**
+ * main - pointers_arrays_strings
+ * the program prints a[2] = 98, followed by a new line
+ * Return: return a[2] = 98
+*/
+
+int main(void)
 {
 	int n;
 	int a[5];
@@ -10,14 +16,12 @@ it main(void)
 	p = &n;
 
 /**
- * write your line of code here...
- * Remember:
+ * main - write your line of code before ; and Remember:
  * - you are not allowed to use a
  * - you are not allowed to modify p
  * - only one statement
  * - you are not allowed to code anything else than this line of code
 */
-
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
