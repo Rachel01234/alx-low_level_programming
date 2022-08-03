b98da9821c9ef13ab2b29f9159badc0c37dcb426
“Rachel01234cat README.mdgit config --global user.name “Rachel01234 <som63625@gmail.com>
2022-08-03 13:51:36 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
-#include"stdlib.h"
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - prints a name
- * @name: pointer to char
- * @f: pointer to function that returns nothing
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
  * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && *name != '\0' && f != NULL)
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
